7cce8a824089bf7fcadf76674eaefc6a00077323
phamvangjang <giangphamvan48@gmail.com>
2023-04-09 18:02:20 +0700

    update Bai2.3

--- src/Phan2Bai3/main.java
+++ src/Phan2Bai3/main.java
@@ -1,62 +1,72 @@
 package Phan2Bai3;
 
 public class main {
-	private int[][] arr;
-	private int n;
-	private int m;
-	
-	public main(int[][] arr) {
-		this.arr=arr;
-		this.n=arr.length;
-		this.m=arr[0].length;
-	}
-	
 //	1) Tìm số lẻ lớn thứ 2 trong mảng
-	public int findSecondLargestOdd(int[][] arr) {
-		int largest = Integer.MAX_VALUE;
-		int secondLargest = Integer.MIN_VALUE;
-		
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				int num=arr[i][j];
+	public static int findSecondLargestOdd(int[][] arr) {
+		int largestOdd=Integer.MIN_VALUE;
+		int secondLargestOdd = Integer.MIN_VALUE;
+		for(int[] row : arr) {
+			for(int num : row) {
 				if(num%2!=0) {
-					if(num>largest) {
-						secondLargest=largest;
-						largest=num;
-					}else if(num>secondLargest&&num!=largest) {
-						secondLargest=num;
+					if(num>largestOdd) {
+						secondLargestOdd=largestOdd;
+						largestOdd=num;
+					}else if(num>secondLargestOdd&&num!=largestOdd) {
+						secondLargestOdd=num;
 					}
 				}
 			}
 		}
-		return secondLargest;
+		return secondLargestOdd;
 	}
 	
 //	2) Tìm phần tử lớn nhất trên 2 đường chéo
-	public int findMaxOnDiagonals() {
-		int max = Integer.MIN_VALUE;
-//		Tìm phần tử lớn nhất trên 2 đường chéo chính
-		for(int i=0;i<Math.min(n, m);i++) {
-			if(arr[i][i]>max) {
-				max=arr[i][i];
+	public static int findMaxOnDiagonals(int[][] arr) {
+		int maxOnDiagonals =Integer.MIN_VALUE;
+		int numRows = arr.length;
+		int numCols = arr[0].length;
+		for(int i=0;i<numRows;i++) {
+			if(i<numCols&&arr[i][i]>maxOnDiagonals) {
+				maxOnDiagonals=arr[i][i];
+			}
+			if(i<numCols&&arr[i][numCols-i-1]>maxOnDiagonals) {
+				maxOnDiagonals=arr[i][numCols-i-1];
 			}
 		}
-//		Tìm phần tử lớn nhất trên 2 đường chéo phụ
-		for(int i=0;i<Math.min(n, m);i++) {
-			if(arr[i][m-i-1]>max) {
-				max=arr[i][m-i-1];
+		return maxOnDiagonals;
+	}
+	
+//	3) Thay thế các phần tử bị thiếu (phần tử có giá trị = -1) trên 1 cột bằng phần tử 	lớn nhất trên cột đó
+	public static void replaceMissingElements(int[][] arr, int col) {
+		int maxOnCol = Integer.MIN_VALUE;
+		for(int[] row : arr) {
+			if(row[col] != -1 && row[col]>maxOnCol) {
+				maxOnCol=row[col];
 			}
 		}
-		return max;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i][col]==-1) {
+				arr[i][col]=maxOnCol;
+			}
+		}
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[][] arr = { { 5,  6,  7, -1}, 
-			            {-1, 19,  3,  8}, 
-			            { 3,  4, -1,  2}, 
-			            { 5,  8,  1, -1}};
-//		bug fixing
-//		System.out.println("Số lẻ lớn thứ 2 trong mảng là: "+findSecondLargestOdd(arr));
+		int[][] arr = {{1, 2, 3, -1},
+			           {4, -1, 6, 7},
+			           {8, 9, 10, 11},
+			          {12, 13, 14, 15}};
+		
+		System.out.println("Số lẻ lớn thứ 2 trong mảng là: "+findSecondLargestOdd(arr));
+		System.out.println("phần tử lớn nhất trên 2 đường chéo: "+findMaxOnDiagonals(arr));
+		
+		main.replaceMissingElements(arr, 1);
+		System.out.println("Mang sau khi duoc thay the la: ");
+		for(int row[] : arr) {
+			for(int num : row) {
+				System.out.print(num+"   ");
+			}
+			System.out.println();
+		}
 	}
 
 }
