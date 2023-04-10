62f1fd344fd1585683b0425e35feaab6ffb78072
phamvangjang <giangphamvan48@gmail.com>
2023-04-10 12:03:03 +0700

    add file Bai2.4

---
+++ src/Phan2Bai4/main.java
@@ -0,0 +1,10 @@
+package Phan2Bai4;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = { { 1, 2, 3, -1 }, { 4, -1, 6, 7 }, { 8, 9, 10, 11 }, { 12, 13, 14, 15 } };
+	}
+
+}
