7b1a5e259ac4867cc8f2a6ad18842d58f20ac5fe
phamvangjang <giangphamvan48@gmail.com>
2023-04-11 22:38:09 +0700

    update file index2.6

--- src/Phan2Bai6/main.java
+++ src/Phan2Bai6/main.java
@@ -1,14 +1,16 @@
 package Phan2Bai6;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class main {
 //	1) Có bao nhiêu ký tự số trong mảng 2 chiều
-	public static int countCharIsNum(char[][] arr) {
+	public static int countDigits(char[][] arr) {
 		int count = 0;
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
-				if (arr[i][j] >= '0' && arr[i][j] <= '9') {
+				if (Character.isDigit(arr[i][j])) {
 					count++;
 				}
 			}
@@ -18,12 +20,40 @@ public class main {
 
 //	2) Chuyển tất cả ký tự chữ viết thường trên 1 cột cho trước sang ký tự viết hoa
 	public static void uppercaseForCols(char[][] arr) {
-		int cols = 0;
+		int cols = 2;
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i][cols] >= 'a' && arr[i][cols] <= 'z') {
-				Character.toUpperCase(arr[i][cols]);
+			if (Character.isLowerCase(arr[i][cols])) {
+				arr[i][cols] = Character.toUpperCase(arr[i][cols]);
 			}
 		}
+	}
+
+//	3) Tìm ký tự xuất hiện nhiều nhất trên 2 đường chéo
+	public static char findMostFrequentCharacter(char[][] arr) {
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		int maxCount = 0;
+		char mostFrequentChar = ' ';
+		// count characters in the main diagonal
+		for (int i = 0; i < arr.length; i++) {
+			char c = arr[i][i];
+			int count = map.getOrDefault(c, 0) + 1;
+			map.put(c, count);
+			if (count > maxCount) {
+				maxCount = count;
+				mostFrequentChar = c;
+			}
+		}
+		// count characters in the secondary diagonal
+		for (int i = 0; i < arr.length; i++) {
+			char c = arr[i][arr.length - i - 1];
+			int count = map.getOrDefault(c, 0) + 1;
+			map.put(c, count);
+			if (count > maxCount) {
+				maxCount = count;
+				mostFrequentChar = c;
+			}
+		}
+		return mostFrequentChar;
 	}
 
 	public static void main(String[] args) {
@@ -31,7 +61,7 @@ public class main {
 		char[][] arr = { { 'p', 'h', 'a', 'm', 'v' }, { 'a', 'n', 'g', 'i', 'g' }, { 'i', 'a', 'n', 'g', 'C' },
 				{ 'N', 'T', 'T', 'K', '6' }, { '2', 'H', 'c', 'M', 'c' }, };
 		int cols = 2;
-		System.out.println("so ký tự số trong mảng: " + countCharIsNum(arr));
+		System.out.println("so ký tự số trong mảng: " + countDigits(arr));
 
 		char[][] str = new char[arr.length][];
 		for (int i = 0; i < arr.length; i++) {
@@ -45,7 +75,7 @@ public class main {
 		System.out.println(Arrays.deepToString(arr));
 		System.out.println("Mảng sau khi thay thế là:");
 		System.out.println(Arrays.deepToString(str));
-//		System.out.println(arr[0][2]);
-//		System.out.println("" + Character.toUpperCase(arr[0][2]));
+
+		System.out.println("ký tự xuất hiện nhiều nhất trên 2 đường chéo: " + findMostFrequentCharacter(arr));
 	}
 }
