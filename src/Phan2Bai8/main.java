63c25199f728193f53d95f2a90e9b300ead0df2e
phamvangjang <giangphamvan48@gmail.com>
2023-04-12 08:45:12 +0700

    test code

--- src/Phan2Bai8/main.java
+++ src/Phan2Bai8/main.java
@@ -1,6 +1,8 @@
 package Phan2Bai8;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class main {
 //	1) Có bao nhiêu phần tử biên (ở hàng/cột đầu tiên hoặc hàng/cột cuối cùng) là ký tự số
@@ -32,7 +34,7 @@ public class main {
 		}
 		return count;
 	}
-
+//	2) Chuyển các kí tự chữ viết thường ở các vị trí biên thành các kí tự viết hoa
 	public static void convertToUppercaseBoundary(char[][] arr) {
 		// hàng đầu tiên
 		for (int j = 0; j < arr[0].length; j++) {
@@ -59,26 +61,44 @@ public class main {
 			}
 		}
 	}
-
+//	3) Tìm ký tự số xuất hiện nhiều nhất trong mảng
+//	public static int findMostFrequentChar(char[][] arr) {
+//		// Tạo một HashMap để đếm số lần xuất hiện của các ký tự
+//		Map<Character, Integer> charCounts = new HashMap<Character, Integer>();
+//		// Duyệt các phần tử của mang
+//		for (int i = 0; i < arr.length; i++) {
+//			for(int j=0;j<arr[0].length;j++) {
+//				charCounts.put(arr[i][j], charCounts.getOrDefault(arr[i][j], 0) + 1);
+//			}
+//		}
+//		return charCounts
+//	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char[][] arr = { { 'p', 'h', 'a', 'm', 'v' }, { 'a', 'n', 'g', 'i', 'g' }, { 'i', 'a', 'n', 'g', 'C' },
-				{ 'N', 'T', 'T', 'K', '6' }, { '2', 'H', 'c', 'M', 'c' } };
+		char[][] arr = { { 'p', 'h', 'a', 'm', 'v' }, 
+						 { 'a', 'n', 'g', 'i', 'g' }, 
+						 { 'i', 'a', 'n', 'g', 'C' },
+						 { 'N', 'T', 'T', 'K', '6' }, 
+						 { '2', 'H', '6', 'M', 'c' } };
 //		bug fixing
-		System.out.println("Số phần tử biên có kí tự là số là: " + countIsDigitBoundary(arr));
+//		System.out.println("Số phần tử biên có kí tự là số là: " + countIsDigitBoundary(arr));
+//		
+//		char[][] str = new char[arr.length][];
+//		for (int i = 0; i < arr.length; i++) {
+//			str[i] = new char[arr[i].length];
+//			for (int j = 0; j < arr[i].length; j++) {
+//				str[i][j] = arr[i][j];
+//			}
+//		}
+//		main.convertToUppercaseBoundary(str);
+//		System.out.println("Mảng trước khi thay thế là:");
+//		System.out.println(Arrays.deepToString(arr));
+//		System.out.println("Mảng sau khi thay thế là:");
+//		System.out.println(Arrays.deepToString(str));
 		
-		char[][] str = new char[arr.length][];
-		for (int i = 0; i < arr.length; i++) {
-			str[i] = new char[arr[i].length];
-			for (int j = 0; j < arr[i].length; j++) {
-				str[i][j] = arr[i][j];
-			}
-		}
-		main.convertToUppercaseBoundary(str);
-		System.out.println("Mảng trước khi thay thế là:");
-		System.out.println(Arrays.deepToString(arr));
-		System.out.println("Mảng sau khi thay thế là:");
-		System.out.println(Arrays.deepToString(str));
+		String a="giang";
+		String b="Giang";
+		System.out.println("result: "+a.equals(b));
 	}
 
 }
